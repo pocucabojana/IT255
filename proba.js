b066ff3e520caa606874049ce5b264c6a62759c1
pocucabojana <pocucabojana@gmail.com>
2016-04-08 21:04:20 +0200

    Delete proba.js

--- proba.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8"
-    }).then(function(data) {
-       $('.rand').append(data.data);
-    });
-});
